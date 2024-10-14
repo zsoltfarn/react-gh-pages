66c83bfd8ea51bafee6d1cb99c480792a27b6bfb
Zsolt Farnas <zsolt.farnas@gmail.com>
2024-10-14 14:43:13 +0200

    Configure React app for deployment to GitHub Pages

--- src/Header.js
+++ src/Header.js
@@ -14,10 +14,10 @@ function Header() {
             <a href="#">Home</a>
           </li>
           <li>
-            <a href="/about">About</a>
+            <a href="#">About</a>
           </li>
           <li>
-            <a href="/contact">Contact</a>
+            <a href="#">Contact</a>
           </li>
         </ul>
       </nav>
